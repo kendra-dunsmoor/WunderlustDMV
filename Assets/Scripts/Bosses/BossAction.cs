c47a638a19c25579505c6de521ea8b0addf846cf
Joe Young <jlyoungthe3rd@gmail.com>
2025-06-06 20:48:25 -0700

    Added scriptable objects for BossActions, BossData

---
+++ Assets/Scripts/Bosses/BossAction.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BossAction", menuName = "Scriptable Objects/BossAction")]
+public class BossAction : ScriptableObject
+{
+    public string BossActionName;
+    public string generalDescription;
+
+    [Header("------------- Action Effects -------------")]
+    public float WILL_MODIFIER;
+    public float PERFORMANCE_MODIFIER;
+    public float BOSS_WILL_MODIFIER;
+    public enum BossActionType
+    {
+        PASSIVE,
+        NEGATIVE,
+        POSTIVE,
+        NEUTRAL
+    }
+
+    // TODO: add accuracy check:
+    public float ACTION_CHANCE = 1.0f; // The chance of the action happening, 1.0f = 100%
+
+    // TODO: add value for conditions and for how many turns the condition lasts
+
+    // TODO: add a function that accepts unique actions
+
+    public string GetDescription()
+    {
+        string description = generalDescription;
+        if (WILL_MODIFIER > 0) description += "Gain " + WILL_MODIFIER + "Will";
+        if (WILL_MODIFIER < 0) description += "Lose " + WILL_MODIFIER + "Will";
+        if (PERFORMANCE_MODIFIER > 0) description += "Gain " + PERFORMANCE_MODIFIER + "Performance";
+        if (PERFORMANCE_MODIFIER < 0) description += "Lose " + PERFORMANCE_MODIFIER + "Performance";
+        if (BOSS_WILL_MODIFIER > 0) description += "Gain " + BOSS_WILL_MODIFIER + "% Boss Will";
+        if (BOSS_WILL_MODIFIER < 0) description += "Lose " + BOSS_WILL_MODIFIER + "% Boss Will";
+        // foreach (ActionEffectStacks effectStacks in effects)
+        // {
+        //     if (effectStacks != null) description += " Adds effect " + effectStacks.effect.type + " for " + effectStacks.stacks + " turns.";
+        // }
+        return description;
+    }
+}
